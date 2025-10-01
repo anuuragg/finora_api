c18233868cf9852f0b5be8270decffef63f82355
Anurag <anuragcpyadav031@gmail.com>
2025-10-01 12:47:02 +0530

    Fix income src controller/routes

--- controllers/incomeSrcController.js
+++ controllers/incomeSrcController.js
@@ -1,25 +1,16 @@
 const IncomeSrc = require("../models/incomeSrcModel");
 const dbConnect = require("../lib/dbConnect");
-
-class CustomError extends Error {
-  error;
-  status;
-  constructor(err, status) {
-    super(err);
-    this.status = status;
-  }
-}
+const CustomError = require('../lib/customError');
 
 const getIncomeSrcAll = async (req, res) => {
   try {
-    const userId = req.user.id;
     await dbConnect();
-    if (!userId) return res.status(404).json({ error: "User not found!" });
+    const userId = req.user.id;
     const income_src = await IncomeSrc.find({ user_id: userId });
-    if (!income_src) throw new CustomError("No income sources found!", 404);
-    res.status(201).json({ income_src });
+    if (income_src.length === 0) throw new CustomError("No income sources found!", 404);
+    res.status(200).json({ income_src });
   } catch (err) {
-    res.status(err.status).json({ error: err.message });
+    res.status(err.status || 500).json({ error: err.message });
   }
 };
 
@@ -27,10 +18,10 @@ const getIncomeSrc = async (req, res) => {
     try{
         await dbConnect();
         const income_src = await IncomeSrc.findById(req.params.id);
-        if (!income_src) return res.status(404).json({error: "No income source found!"});
+        if (income_src.length === 0) throw new CustomError("No income source found!", 404)
         res.status(200).json(income_src);
     } catch(err){
-        res.status(500).json({error: err.message})
+        res.status(err.status || 500).json({ error: err.message });
     }
 }
 
@@ -39,60 +30,39 @@ const addIncomeSrc = async (req, res) => {
     await dbConnect();
     const userId = req.user.id;
     const incomeSrc = req.body.income_src;
-    if (!userId) return res.status(404).json({ error: "User not found!" });
-    if (!incomeSrc)
-      return res.status(404).json({ error: "No income source was entered" });
+    if (!incomeSrc) throw new CustomError("No income source was entered", 400);
     const income_src = await IncomeSrc.create({
       user_id: userId,
       income_src: incomeSrc,
     });
-    if (!income_src)
-      return res
-        .status(500)
-        .json({ error: "Unable to create the income source!" });
+    if (income_src.length === 0) throw new CustomError("Unable to create the income source!", 500);
     res.status(201).json({ income_src });
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.status(err.status || 500).json({ error: err.message });
   }
 };
 
 const updateIncomeSrc = async (req, res) => {
   try {
     await dbConnect();
-    const { old_income_src, new_income_src } = req.body;
-    if (!userId) return res.status(404).json({ error: "User not found!" });
-    if (!old_income_src)
-      return res.status(404).json({ error: "Old income source missing" });
-    if (!new_income_src)
-      return res
-        .status(404)
-        .json({ error: "No new income source was entered" });
-    const updatedIncomeSrc = await IncomeSrc.findOneAndUpdate(
-      { user_id: req.params.id, income_src: old_income_src },
-      { income_src: new_income_src },
-      { new: true, runValidators: true }
-    );
-    if (!updatedIncomeSrc)
-      return res
-        .status(500)
-        .json({ error: "Unable to update the income source!" });
+    const income_src = req.body;
+    if (!income_src) throw new CustomError("No income source was entered to update", 400);
+    const updatedIncomeSrc = await IncomeSrc.findByIdAndUpdate(req.params.id, income_src, { new: true });
+    if (updatedIncomeSrc.length === 0) throw new CustomError("Unable to update the income source!", 404);
     res.status(200).json({ updatedIncomeSrc });
   } catch (err) {
-    res.json(500).json({ error: err.message });
+    res.status(err.status || 500).json({ error: err.message });
   }
 };
 
 const deleteIncomeSrc = async (req, res) => {
   try {
     await dbConnect();
-    const deleted_src = await IncomeSrc.findOneAndDelete({
-      income_src: req.params.income_src,
-    });
-    if (!deleted_src)
-      return res.status(500).json({ error: "Income source not found!" });
-    res.status(201).json({ deleted_src });
+    const deleted_src = await IncomeSrc.findByIdAndDelete(req.params.id);
+    if (deleted_src.length === 0) throw new CustomError("Income source not found!", 404)
+    res.status(200).json({ deleted_src });
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.status(err.status || 500).json({ error: err.message });
   }
 };
 
