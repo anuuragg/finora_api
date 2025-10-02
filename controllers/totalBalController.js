3d599b353b7cbd8a01cc2b8cdf515513cddb0493
Anurag <anuragcpyadav031@gmail.com>
2025-10-02 15:33:00 +0530

    Fix total balance controller/route

--- controllers/totalBalController.js
+++ controllers/totalBalController.js
@@ -3,6 +3,7 @@ const Expenses = require('../models/expenseModel');
 const CustomError = require('../lib/customError');
 const dbConnect = require('../lib/dbConnect');
 const buildDateFilter = require('../lib/dateFilter');
+const calculateTotals = require('../lib/calculateTotals');
 
 const getRecords = async (req, res) => {
     try{
@@ -11,29 +12,13 @@ const getRecords = async (req, res) => {
         const {days} = req.body;
         let dateFilter = buildDateFilter(days);
 
-        const [incomeRecords, expenseRecords] = await Promise.all([
-        Income.find({ user_id: userId, ...dateFilter }),
-        Expenses.find({ user_id: userId, ...dateFilter })
+        const [incomeRecords, expenseRecords, subCatRecords] = await Promise.all([
+            Income.find({ user_id: userId, ...dateFilter }),
+            Expenses.find({ user_id: userId, ...dateFilter })
+            .populate("sub_category_id", "sub_cat_name -_id")
         ]);
-
-        const total_income = incomeRecords.reduce((sum, record) => sum + record.amount, 0);
-        const total_expenses = expenseRecords.reduce((sum, record) => sum + record.amount, 0);
-        
-        const total_savings = expenseRecords.reduce((sum, record) => record.category === 'savings' ? sum + record.amount : sum, 0);
-        const total_needs = expenseRecords.reduce((sum, record) => record.category === 'needs' ? sum + record.amount : sum, 0);
-        const total_wants = expenseRecords.reduce((sum, record) => record.category === 'wants' ? sum + record.amount : sum, 0);
-        const total_investments = expenseRecords.reduce((sum, record) => record.category === 'investments' ? sum + record.amount : sum, 0);
-
-        const total = {
-            total_income: total_income,
-            total_expenses: total_expenses,
-            total_needs: total_needs,
-            total_wants: total_wants,
-            total_investments: total_investments,
-            total_savings: total_savings,
-            total_balance: total_income - total_expenses
-        }
-
+        console.log(expenseRecords)
+        const total = calculateTotals(incomeRecords, expenseRecords);
         res.status(200).json(total);
     } catch(err){
         res.status(err.status || 500).json({error: err.message});
@@ -51,25 +36,7 @@ const getSpecificRecord = async (req, res) => {
         Expenses.find({ user_id: userId, date: parsedDate })
         ]);
 
-        const total_income = incomeRecord.reduce((sum, record) => sum + record.amount, 0);
-        const total_expenses = expenseRecord.reduce((sum, record) => sum + record.amount, 0);
-        
-        const total_savings = expenseRecord.reduce((sum, record) => record.category === 'savings' ? sum + record.amount : sum, 0);
-        const total_needs = expenseRecord.reduce((sum, record) => record.category === 'needs' ? sum + record.amount : sum, 0);
-        const total_wants = expenseRecord.reduce((sum, record) => record.category === 'wants' ? sum + record.amount : sum, 0);
-        const total_investments = expenseRecord.reduce((sum, record) => record.category === 'investments' ? sum + record.amount : sum, 0);
-
-        const total = {
-            total_income: total_income,
-            total_expenses: total_expenses,
-            total_needs: total_needs,
-            total_wants: total_wants,
-            total_investments: total_investments,
-            total_savings: total_savings,
-            total_balance: total_income - total_expenses
-        }
-
-
+        const total = calculateTotals(incomeRecord, expenseRecord);
         res.status(200).json(total);
     } catch(err){
         res.status(err.status || 500).json({error: err.message});
